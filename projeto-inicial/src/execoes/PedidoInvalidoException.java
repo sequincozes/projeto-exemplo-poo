41b7a3c7b2ba81d50c2db9a79003d1c096428fa3
Silvio Quincozes <sequincozes@gmail.com>
2026-09-13 23:09:50 -0300

    Update PedidoInvalidoException.java
    Trocando a mensagem do erro do pedido;

--- projeto-inicial/src/execoes/PedidoInvalidoException.java
+++ projeto-inicial/src/execoes/PedidoInvalidoException.java
@@ -2,6 +2,6 @@ package execoes;
 
 public class PedidoInvalidoException extends RuntimeException {
     public PedidoInvalidoException(String messagem){
-        super(messagem);
+        super(messagem + " - Vish");
     }
 }
